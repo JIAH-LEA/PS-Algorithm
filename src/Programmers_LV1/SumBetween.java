5e18b879fdfcad99cc6f08514be198f534642024
이지아 <leeahjiahlea@sookmyung.ac.kr>
2020-08-26 22:00:05 +0900

    두 정수 사이의 합

---
+++ src/Programmers_LV1/SumBetween.java
@@ -0,0 +1,25 @@
+package Programmers_LV1;
+
+class SumBetween {
+    public static void  main(String[] args){
+       SumBetween s = new SumBetween();
+       System.out.println(s.sumof(3,5));
+    }
+
+    public long sumof(int a, int b) {
+        long answer = 0;
+        if(a==b)
+            return a;
+
+        else if(a<b){
+            for(int i=a; i<=b; i++)
+                answer+=i;
+        }
+        else if(b<a){
+            for(int i=b; i<=a; i++)
+                answer+=i;
+        }
+        return answer;
+    }
+}
+
