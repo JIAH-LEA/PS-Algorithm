5ff1b32563bb24d2a3ed8c17f5ee1e9ff2a43fb5
이지아 <leeahjiahlea@sookmyung.ac.kr>
2020-08-26 22:02:28 +0900

    두 정수 사이의 합

--- src/Programmers_LV1/SumBetween.java
+++ src/Programmers_LV1/SumBetween.java
@@ -1,23 +1,22 @@
 package Programmers_LV1;
 
 class SumBetween {
-    public static void  main(String[] args){
-       SumBetween s = new SumBetween();
-       System.out.println(s.sumof(3,5));
+    public static void main(String[] args) {
+        SumBetween s = new SumBetween();
+        System.out.println(s.sumof(3, 5));
     }
 
     public long sumof(int a, int b) {
         long answer = 0;
-        if(a==b)
+        if (a == b)
             return a;
 
-        else if(a<b){
-            for(int i=a; i<=b; i++)
-                answer+=i;
-        }
-        else if(b<a){
-            for(int i=b; i<=a; i++)
-                answer+=i;
+        else if (a < b) {
+            for (int i = a; i <= b; i++)
+                answer += i;
+        } else {
+            for (int i = b; i <= a; i++)
+                answer += i;
         }
         return answer;
     }
